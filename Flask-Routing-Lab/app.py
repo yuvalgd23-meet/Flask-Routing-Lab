aa8e4786904da05b1a17e2386c382a40bf57897b
yuvalgd23-meet <109908289+yuvalgd23-meet@users.noreply.github.com>
2022-07-26 14:18:50 +0300

    1

--- Flask-Routing-Lab/app.py
+++ Flask-Routing-Lab/app.py
@@ -7,12 +7,22 @@ app = Flask(  # Create a flask app
     static_folder='static'  # Name of directory for static files
 )
 
-# Your code should be below
+# Your code shod be below
 
+@app.route('/')
+def home():
+    return render_template('home.html')
 
+@app.route('/2nd-R')
+def cart():
+    return render_template('cart.html')
+
+@app.route('/3rd-R')
+def product():
+    return render_template('product.html')
 
 
 # Your code should be above
 
-if __name__ == "__main__":  # Makes sure this is the main process
+if __name__ == '__main__':  # Makes sure this is the main process
     app.run(debug=True)
